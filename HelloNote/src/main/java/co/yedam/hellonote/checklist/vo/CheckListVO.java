1a22a4f4fde191eb72cdd993350828ae51a8ca08
origin9 <kwj01230@gmail.com>
2019-12-12 09:14:28 +0900

    just only checkList revise

---
+++ HelloNote/src/main/java/co/yedam/hellonote/checklist/vo/CheckListVO.java
@@ -0,0 +1,12 @@
+package co.yedam.hellonote.checklist.vo;
+
+import lombok.Data;
+
+@Data
+public class CheckListVO {
+	String checklistMission;
+	String checklistStatus;
+	String menuId;
+	int checklistSeq;
+
+}

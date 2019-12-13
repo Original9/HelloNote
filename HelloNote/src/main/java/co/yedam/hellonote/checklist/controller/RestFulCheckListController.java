afbe15aa0069f77f7026a6b76564495812b7ad14
sjk98027 <kakrkddusdn@naver.com>
2019-12-13 15:49:23 +0900

    check리스트 insert update 완료 delete와 화면작업 남음(sql-map-config-spring.xml)수정작업 했음.

---
+++ HelloNote/src/main/java/co/yedam/hellonote/checklist/controller/RestFulCheckListController.java
@@ -0,0 +1,57 @@
+package co.yedam.hellonote.checklist.controller;
+
+import java.io.Console;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.yedam.hellonote.calendar.vo.CalendarVO;
+import co.yedam.hellonote.checklist.service.CheckListService;
+import co.yedam.hellonote.checklist.vo.CheckListVO;
+
+@RestController
+public class RestFulCheckListController {
+
+	@Autowired CheckListService checkListService;
+	
+	@RequestMapping(value="/getCheckList",method=RequestMethod.GET)
+	public List<CheckListVO> getCheckList(HttpServletRequest request,Model model, CheckListVO vo){
+		UserDetails userDetails =(UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		HttpSession session = request.getSession();
+		vo.setHellonoteId(userDetails.getUsername());
+		vo.setMenuId((String)session.getAttribute("menuId"));
+		return checkListService.getCheckList(vo);
+	}
+	
+	
+	@RequestMapping(value="/insertList",method=RequestMethod.POST ,consumes ="application/json")
+	public CheckListVO insertList(@RequestBody CheckListVO vo,HttpSession session ) {
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		vo.setHellonoteId(userDetails.getUsername());
+		vo.setMenuId((String)session.getAttribute("menuId"));
+		checkListService.insertList(vo);
+		return vo;
+	}
+	
+	
+	@RequestMapping(value="/updateChecklist",method=RequestMethod.PUT ,consumes ="application/json")
+	public CheckListVO updateChecklist(@RequestBody CheckListVO vo,HttpSession session ) {
+		System.out.println("controller");
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		vo.setHellonoteId(userDetails.getUsername());
+		vo.setMenuId((String)session.getAttribute("menuId"));
+		checkListService.updateList(vo);
+		return vo;
+	}
+	
+}

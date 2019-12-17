45a371ffd90804a107cb34d799446cfd0b182351
bs930702 <bs930702@gmail.com>
2019-12-17 16:40:13 +0900

    dd

--- HelloNote/src/main/java/co/yedam/hellonote/user/controller/UserController.java
+++ HelloNote/src/main/java/co/yedam/hellonote/user/controller/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package co.yedam.hellonote.user.controller;
 
 import java.io.IOException;
@@ -58,7 +57,7 @@ public class UserController {
 		userService.deleteUserProc(vo);
 		return "redirect:getUserList";
 	}
-	
+
 	// 회원 탈퇴
 	@RequestMapping("selfDeleteUser")
 	public String uesrdeleteuser(UserVO vo) {
@@ -167,172 +166,8 @@ public class UserController {
 	public UserVO ChangeInfo(@RequestBody UserVO vo) {
 		userService.updateUserChange(vo);
 		// 세션에 사용자 정보를 다시 변경
-		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(vo, null, vo.getAuthorities()));
+		SecurityContextHolder.getContext()
+				.setAuthentication(new UsernamePasswordAuthenticationToken(vo, null, vo.getAuthorities()));
 		return userService.getUser(vo);
 	}
 }
-=======
-package co.yedam.hellonote.user.controller;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import co.yedam.hellonote.user.service.UserService;
-import co.yedam.hellonote.user.vo.UserVO;
-
-@Controller
-public class UserController {
-	@Autowired
-	UserService userService;
-
-	// 아이디 중복체크
-	@ResponseBody
-	@RequestMapping(value = "idCheck", method = RequestMethod.POST)
-	public int postIdCheck(HttpServletRequest request) throws Exception {
-		String hellonoteId = request.getParameter("hellonoteId");
-		UserVO idCheck = userService.idCheck(hellonoteId);
-		System.out.println(idCheck + "@@@@@@@@@@@@@");
-		int result = 0;
-
-		if (idCheck != null) {
-			result = 1;
-		}
-		return result;
-	}
-
-	// 회원가입 등록처리 프로시저
-	@RequestMapping("insertUserSignUp")
-	public String insertUserSignUp(UserVO vo) {
-		vo.sethGrant("U");
-		userService.insertUserSignUp(vo);
-		return "redirect:/";
-	}
-
-	// 프로시저 단건삭제
-	@RequestMapping("deleteUserProc")
-	public String deleteUserProc(UserVO vo) {
-		userService.deleteUserProc(vo);
-		return "redirect:getUserList";
-	}
-
-	// 프로시저 리스트 삭제 처리
-	@RequestMapping("deleteUserListProc")
-	public String deleteUserListProc(@RequestParam String[] rowCheck, UserVO vo) {
-		userService.deleteUserListProc(rowCheck, vo);
-		return "redirect:getUserList";
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String homelogin(Locale locale, Model model) {
-
-		return "layout/login";
-	}
-
-	@RequestMapping(value = "signup", method = RequestMethod.GET)
-	public String signup(Locale locale, Model model) {
-
-		return "user/signup";
-	}
-
-	@RequestMapping(value = "/mainPage", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		return "main/main/mainPage";
-	}
-
-	// 프로필이동
-	@RequestMapping("getProfile")
-	public String getProfile(Model model, UserVO vo) {
-		model.addAttribute("user", userService.getUserList(vo));
-		return "main/user/profile"; // jsp 경로
-	}
-
-	// 관리자 페이지 이동
-	@RequestMapping(value = { "/admin/*" }, method = RequestMethod.GET)
-	public String showIssueList(HttpServletRequest request) {
-		// url 값을 받아와서 * 에 넣어줘야 한다.
-		String uri = request.getRequestURI();
-		String context = request.getContextPath();
-		String path = uri.substring(context.length());
-		String arrayOfUri[] = path.split("/");
-		String x = "main/" + arrayOfUri[1] + "/" + arrayOfUri[2];
-		return x;
-	}
-
-	// 유저 리스트 페이지
-	@RequestMapping("getUserList")
-	public String getUserList(Model model, UserVO vo) {
-		model.addAttribute("user", userService.getUserList(vo));
-		return "main/user/userlist"; // jsp 경로
-	}
-
-	// 삭제 처리
-	@RequestMapping("deleteUser")
-	public String deleteUser(@RequestParam String[] rowCheck, UserVO vo) {
-		userService.deleteUser(rowCheck, vo);
-		return "redirect:getUserList";
-	}
-
-	// 엑셀출력
-	@RequestMapping("downloadExcel1")
-	public ModelAndView excelView(UserVO vo) throws IOException {
-		List<Map<String, Object>> list = userService.getUserListMap(vo);
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		String[] header = { "hellonoteId", "gender", "hGrant", "age", "hProfile" };
-		map.put("headers", header);
-		map.put("filename", "excel_userlist");
-		map.put("datas", list);
-		return new ModelAndView("commonExcelView", map);
-	}
-
-	// 단건 조회
-	@ResponseBody
-	@RequestMapping("getUser")
-	public UserVO getUser(UserVO vo) {
-		return userService.getUser(vo);
-	}
-
-	// 단건 삭제
-	@RequestMapping("getUserdelete")
-	public String getUserdelete(UserVO vo) {
-		userService.getUserdelete(vo);
-		return "redirect:getUserList";
-	}
-
-	// 수정
-	@RequestMapping(value = "updateUser", method = RequestMethod.PUT, consumes = "application/json" // 요청헤더
-	)
-	@ResponseBody // return 값이 java객체를 json 구조로 바꿔준다 @RequestBody는 반대로
-	public UserVO updateUser(@RequestBody UserVO vo) {
-		userService.updateUser(vo);
-		return userService.getUser(vo);
-	}
-
-	@RequestMapping(value = "ChangeInfo", method = RequestMethod.PUT, consumes = "application/json" // 요청헤더
-	)
-	@ResponseBody // return 값이 java객체를 json 구조로 바꿔준다 @RequestBody는 반대로
-	public UserVO ChangeInfo(@RequestBody UserVO vo) {
-		userService.updateUserChange(vo);
-		// 세션에 사용자 정보를 다시 변경
-		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(vo, null, vo.getAuthorities()));
-		return userService.getUser(vo);
-	}
-}
->>>>>>> branch 'master' of https://github.com/Original9/HelloNote
--- HelloNote/src/main/webapp/resources/assets/pageJs/profile.js
+++ HelloNote/src/main/webapp/resources/assets/pageJs/profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // userdatatable Jquery
 $(document).ready(function() {
 	// 수정 버튼 클릭
@@ -67,52 +66,3 @@ function deleteUser() {
 		document.ChangeInfo.submit();
 	}
 }
-=======
-// userdatatable Jquery
-$(document).ready(function() {
-	// 수정 버튼 클릭
-	$('#updatebutton').on('click', function() {
-
-		// span tag는 FORM ID 에 ID .html
-		// input태그는 (form ID input 종류select or input["name=이름"]).val()
-		var pw = $('#ChangeInfo input[name="pw"]').val();
-		var hellonoteId = $('#ChangeInfo input[name="hellonoteId"]').val();
-		var gender = $('#ChangeInfo input[name="gender"]').val();
-		var age = $('#ChangeInfo input[name="age"]').val();
-		var hGrant = $('#ChangeInfo input[name="hGrant"]').val();
-		var hProfile = $('#ChangeInfo textarea[name="hProfile"]').val();
-		
-		
-		// 비밀번호와 비밀번호 확인에 입력된 값이 동일한지 확인
-		if (ChangeInfo.pw.value != ChangeInfo.pwch.value) {
-			alert("비밀번호를 동일하게 입력하세요.")
-			$("#pwch").focus();
-			return false;
-		}
-		
-		$.ajax({
-			url : "ChangeInfo",
-			method : 'PUT',
-			dataType : 'json',
-			data : JSON.stringify({
-				pw : pw,
-				hellonoteId : hellonoteId,
-				hProfile : hProfile,
-				hGrant : hGrant,
-				age : age,
-				gender : gender
-			}),
-			contentType : 'application/json',
-			success : getChangeHandler
-		});
-	});
-
-});
-function getChangeHandler(data) {
-	alert("회원정보 수정이 완료되었습니다.")
-
-	location.reload();
-}
-
-
->>>>>>> branch 'master' of https://github.com/Original9/HelloNote
